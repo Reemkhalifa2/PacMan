796714ff7b6808bc0c1bedc249a2ab4262ce1343
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-22 11:34:19 +0400

    add draw function for foods

--- src/PacMan.java
+++ src/PacMan.java
@@ -146,8 +146,12 @@ public class PacMan extends JPanel {
         g.drawImage(packman.image, packman.x, packman.y,packman.width, packman.height, null);
         for(Block ghost : ghosts){
             g.drawImage(ghost.image, ghost.x, ghost.y,ghost.width, ghost.height, null);
-        }for(Block wall : walls){
+        }
+        for(Block wall : walls){
             g.drawImage(wall.image, wall.x, wall.y,wall.width, wall.height, null);
+        }
+        for(Block food : foods){
+            g.fillRect( food.x, food.y,food.width, food.height);
         }
     }
 
